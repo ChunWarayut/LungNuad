d2d9b30951923a367cb189272167f59b407d2d5e
chunwarayut <warayut2555@gmail.com>
2018-04-17 10:35:07 +0700

    1.0.1

---
+++ src/module/item/item.module.ts
@@ -0,0 +1,45 @@
+export interface Item {
+  key?:string;
+
+  FOOD_ID?: string;
+  FOOD_NAME: string;
+  FOOD_PRICE: number;
+  FOOD_IMG : string;
+  FOOD_TYPE_NAME: string;
+  TEST_TEST_ID: number;
+
+}
+export interface Type {
+
+  key?:string;
+
+  FOOD_TYPE_ID: string;
+  FOOD_TYPE_NAME: string;
+
+}
+
+export interface Detail {
+
+  key?:string;
+
+  DETAIL_ID:string;
+  DETAIL_AMOUT:string;
+  DETAIL_PRICE:string;
+  DETAIL_DATE:string;
+  DETAIL_ADDRESS:string;
+  DETAIL_STATUS:string;
+
+
+}
+
+export interface Buyer {
+
+  key?:string;
+  BUYER_ID:string;
+  BUYER_NAME:string;
+  BUYER_PASSWORD:string;
+  BUYER_ADDRESS:string;
+  BUYER_PHONE:string;
+  BUYER_EMAIL:string;
+
+}
