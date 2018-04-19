21e8d131db2db727c010301879283b0862d945cf
chunwarayut <warayut2555@gmail.com>
2018-04-19 22:01:34 +0700

    190420181001

--- src/pages/menu-list-edit/menu-list-edit.ts
+++ src/pages/menu-list-edit/menu-list-edit.ts
@@ -1,5 +1,11 @@
+import { ServiceProvider } from './../../providers/service/service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Food, Type } from '../../module/item/item.module';
+import { ToastServiceProvider } from '../../providers/toast-service/toast-service';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the MenuListEditPage page.
@@ -15,11 +21,127 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuListEditPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  food:Food;
+
+
+  captureDataUrl: string;
+  alertCtrl: AlertController;
+
+
+  type: Type = {
+
+    FOOD_TYPE_ID  : undefined,
+    FOOD_TYPE_NAME  : undefined
+
+  };
+    
+  TypeList$:Observable<Type[]>;
+
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private fooding: ServiceProvider,
+    private toast:ToastServiceProvider, alertCtrl: AlertController, private camera: Camera, private typeing: ServiceProvider) {
+      
+      this.alertCtrl = alertCtrl;
+
+
+    this.TypeList$ = this.typeing
+    .getTypeList()
+    .snapshotChanges()
+    .map(
+      Change => {
+        return Change.map(c=> ({
+          key : c.payload.key,
+          ...c.payload.val(),
+        }));
+      });
+
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuListEditPage');
+  }
+
+
+  ionViewWillLoad() {
+    this.food = this.navParams.get('food');
+  }
+
+
+  save(food){
+    
+    food.FOOD_TYPE_NAME = this.type.FOOD_TYPE_NAME;
+
+    this.fooding.editFoodItem(food).then(() =>{
+      this.toast.show(`${food.FOOD_NAME} save!`)
+      this.navCtrl.setRoot('HomeAdminPage');});
+  }
+  removeItem(food){
+    this.fooding.removeFoodItem(food).then(()=>{
+      this.toast.show(`${food.FOOD_NAME} deleted!`);
+      this.navCtrl.setRoot('HomeAdminPage');
+    });
+  }
+
+
+ saveDataImg(food){
+
+
+  food.FOOD_IMG = 'F_' + Math.floor(Date.now() / 100);
+  food.FOOD_TYPE_NAME = this.type.FOOD_TYPE_NAME;
+
+  this.fooding.editFoodItem(food).then(() =>{
+
+
+
+    this.toast.show(`${food.FOOD_NAME} save!`)
+    this.navCtrl.setRoot('HomeAdminPage');});
+    this.upload();
+
+
+  }
+
+  selectPhoto(): void {
+    const cameraOptions: CameraOptions = {
+      quality: 10,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      // In this app, dynamically set the picture source, Camera or photo gallery
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    };
+    this.camera.getPicture(cameraOptions).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+  upload() {
+    let storageRef = firebase.storage().ref();
+
+    const filename = this.food.FOOD_IMG;
+
+    // Create a reference to 'images/todays-date.jpg'
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+      this.showSuccesfulUploadAlert();
+    });
+
+  }
+
+  showSuccesfulUploadAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'อัพโหลดสำเร็จ',
+      subTitle: 'Picture is uploaded to Firebase',
+      buttons: ['OK']
+    });
+    alert.present();
+    // clear the previous photo data in the variable
+    this.captureDataUrl = "";
   }
 
 }
--- src/pages/menu-type-edit/menu-type-edit.ts
+++ src/pages/menu-type-edit/menu-type-edit.ts
@@ -1,5 +1,8 @@
+import { Type } from './../../module/item/item.module';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ServiceProvider } from '../../providers/service/service';
+import { ToastServiceProvider } from '../../providers/toast-service/toast-service';
 
 /**
  * Generated class for the MenuTypeEditPage page.
@@ -15,11 +18,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuTypeEditPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  type:Type;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private typeing: ServiceProvider,
+    private toast:ToastServiceProvider) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuTypeEditPage');
+  }
+
+  ionViewWillLoad() {
+    this.type = this.navParams.get('type');
+  }
+
+  
+  save(type){
+    this.typeing.editTypeItem(type).then(() =>{
+      this.toast.show(`${type.FOOD_TYPE_NAME} save!`)
+      this.navCtrl.setRoot('HomeAdminPage');});
+  }
+  removeItem(type){
+    this.typeing.removeTyperItem(type).then(()=>{
+      this.toast.show(`${type.FOOD_TYPE_NAME} deleted!`);
+      this.navCtrl.setRoot('HomeAdminPage');
+    });
   }
 
 }
